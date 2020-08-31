402c2d5ddfb689f4ca115687870946253522cbe4
duzhuo <1638388435@qq.com>
2020-08-31 21:28:08 +0800

    add 美团_零碎知识点

---
+++ 美团/零碎知识点.js
@@ -0,0 +1,5 @@
+// console.log(typeof null);//object
+// console.log(typeof undefined);//undefined
+//1.js的基本数据类型？为什么string是基本数据类型还可以调用数组的那些方法？
+//Undefined、Null、Boolean、Number和String。 还有一种复杂的数据类型Object
+//string是一种特殊的数组，每一个元素都是一个字符
