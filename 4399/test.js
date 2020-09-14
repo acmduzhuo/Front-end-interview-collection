c788cb1ed29c4ee7cee3227b0bd1db729594990f
duzhuo <1638388435@qq.com>
2020-09-14 11:11:57 +0800

    add  8 errors

---
+++ 4399/test.js
@@ -0,0 +1,48 @@
+//1.
+// function foo() {
+// }
+// let old=foo.name;
+// foo.name='bar';
+// console.log(old);
+// console.log(foo.name);
+// foo
+// foo
+//
+
+// function foo() {
+// }
+// foo.n = 'bar';
+// console.log(foo.n);//bar
+
+//2.
+//进制转换
+// var num = 10;
+// console.log(num.toString(16));//a
+// console.log([1,3,4].map(parseInt));//[ 1, NaN, NaN ]
+// console.log(parseInt(11, 2));//3 2进制转为十进制
+// console.log(1.113511.toFixed(3));//1.114
+
+//3.
+//TCP不适用于广播
+
+//4.
+//Height Width的宽度不能为负数，否则就会默认为0
+
+//5.
+//CSS产生空隙的原因：
+//index
+
+//6.
+//offsetWidth 水平方向 width + 左右padding + 左右border-width
+//offsetHeight 垂直方向 height + 上下padding + 上下border-width
+//https://blog.csdn.net/qq_33036599/article/details/81224346
+
+//7.
+//五大浏览器内核：
+//Chrome 之前Webkit 现在Blink
+//Firefox Gecko
+//Safari:Webkit
+//Opera Presto->Webkit->Blink
+//IE Trident
+
+//
