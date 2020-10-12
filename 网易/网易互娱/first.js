7045c3f22b78888087a1deb90062bf055bc354a9
duzhuo <1638388435@qq.com>
2020-10-12 21:31:19 +0800

    update WYHY_first

---
+++ 网易/网易互娱/first.js
@@ -0,0 +1,78 @@
+// let logger = {
+//
+//     level: 'default',
+//
+// log(msg) {
+//
+//     console.log(this.level, msg)
+//
+// }
+//
+// };
+
+
+// 下列每种绑定方式在点击时输出结果是什么
+
+// window.onclick = logger.log
+//
+// window.onclick = (evt) => logger.log(evt)
+//
+// window.onclick = logger.log.bind(logger)
+//
+// window.onclick = () => logger.log.apply(logger, arguments)
+const root = {
+
+    val: 4,
+
+    left: {
+
+        val: 2,
+
+        left: {
+
+            val: 1
+
+        },
+
+        right: {
+
+            val: 3
+
+        }
+
+    },
+
+    right: {
+
+        val: 5,
+
+        right: {
+
+            val: 6,
+
+            right: {
+
+                val: 7
+
+            }
+
+        }
+
+    }
+
+}
+
+
+
+
+function getDepth (root){
+    var sum = 0;
+    if(root){
+        var sum1 = getDepth(root.left);
+        var sum2 = getDepth(root.right);
+        var sum = sum1>sum2 ? sum1+1 : sum2+1;
+    }
+    return sum;
+}
+console.log(getDepth(root));
+
