31c9d1216b4d8055ac1f536548d61e1e72646346
duzhuo <1638388435@qq.com>
2020-10-12 21:31:53 +0800

    update WYHY_first

--- 网易/网易互娱/first.js
+++ 网易/网易互娱/first.js
@@ -20,59 +20,65 @@
 // window.onclick = logger.log.bind(logger)
 //
 // window.onclick = () => logger.log.apply(logger, arguments)
-const root = {
+// const root = {
+//
+//     val: 4,
+//
+//     left: {
+//
+//         val: 2,
+//
+//         left: {
+//
+//             val: 1
+//
+//         },
+//
+//         right: {
+//
+//             val: 3
+//
+//         }
+//
+//     },
+//
+//     right: {
+//
+//         val: 5,
+//
+//         right: {
+//
+//             val: 6,
+//
+//             right: {
+//
+//                 val: 7
+//
+//             }
+//
+//         }
+//
+//     }
+//
+// }
+//
+//
+//
+//
+// function getDepth (root){
+//     var sum = 0;
+//     if(root){
+//         var sum1 = getDepth(root.left);
+//         var sum2 = getDepth(root.right);
+//         var sum = sum1>sum2 ? sum1+1 : sum2+1;
+//     }
+//     return sum;
+// }
+// console.log(getDepth(root));
+const arr = [88, 24, 15, 26];
 
-    val: 4,
-
-    left: {
-
-        val: 2,
-
-        left: {
-
-            val: 1
-
-        },
-
-        right: {
-
-            val: 3
-
-        }
-
-    },
-
-    right: {
-
-        val: 5,
-
-        right: {
-
-            val: 6,
-
-            right: {
-
-                val: 7
-
-            }
-
-        }
-
-    }
-
+for (var i = 0; i < arr.length; i++) {
+    setTimeout(function() {
+        console.log('Index: ' + i + ', element: ' + arr[i]);
+    }, 3000);
 }
-
-
-
-
-function getDepth (root){
-    var sum = 0;
-    if(root){
-        var sum1 = getDepth(root.left);
-        var sum2 = getDepth(root.right);
-        var sum = sum1>sum2 ? sum1+1 : sum2+1;
-    }
-    return sum;
-}
-console.log(getDepth(root));
-
