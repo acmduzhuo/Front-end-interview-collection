fafefb45b082e90c098f1610daab672f2101f00a
duzhuo <1638388435@qq.com>
2020-10-15 21:42:38 +0800

    update water

--- 总结/防抖与节流.js
+++ 总结/防抖与节流.js
@@ -1,15 +1,4 @@
 //防抖
-// function f(fn, delay) {
-//    let timer;
-//    return function () {
-//        let context = this;
-//        let args = arguments;
-//        clearTimeout(timer);
-//        timer = setTimeout(function () {
-//            fn.apply(context, args);
-//        }, delay);
-//    }
-// }
 function f(fn, delay){
     let timer;
     return function () {
@@ -22,21 +11,6 @@ function f(fn, delay){
     }
 }
 //节流
-// let f = function (fn, delay) {
-//     let timer = null;
-//     return function () {
-//         let context = this;
-//         let args = arguments;
-//         if(!timer){
-//             setTimeout(() => {
-//                 fn.apply(context, args);
-//                 clearTimeout(timer);
-//                 timer = null;
-//             }, delay);
-//         }
-//     }
-// }
-
 function f1(fn, delay) {
     var timer = null;
     return function () {
